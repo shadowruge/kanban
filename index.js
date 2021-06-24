862e4950140e6a14159b6608a78a312738cee2d3
izaias <zambelestar@gmail.com>
2021-06-23 23:16:08 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function textareaFunction() {
-        
-    var x = document.createElement("textarea");
-    var t = document.createTextNode(""); //não declare text node
-    x.appendChild(t);
-    document.body.appendChild(x);
-  }
